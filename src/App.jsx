7b60b53547832a4f9a806e4738fe2932d3728f53
Jawad Hassan <jawadhassankhan2001@gmail.com>
2024-05-13 14:16:29 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from './components/ui/button'
+function App() {
+ 
+  return (
+    <>
+    <div>
+      <Button>Click Me</Button>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
